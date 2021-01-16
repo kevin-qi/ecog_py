4aa49e595248279645eb741f25e1dfe12c9ed740
Kevin <kevin.qi@berkeley.edu>
2021-01-16 00:39:07 -0800

    debug dwt

--- ecog_py/dwt.py
+++ ecog_py/dwt.py
@@ -5,10 +5,10 @@ DWT
 Performs discrete wavelet transforms.
 """
 
-import process_nwb
+from process_nwb import wavelet_transform
 import numpy as np
 
-def wavelet_transform(data, fs, fsds=1000, zscore=True):
+def dwt(data, fs, fsds=1000, zscore=True):
     """
     Performs Discrete Wavelet Transform on multi-channel ECoG data. The processing is done per channel to reduce RAM usage.
     
@@ -39,12 +39,15 @@ def wavelet_transform(data, fs, fsds=1000, zscore=True):
     print("Wavelet transforming per channel... (This may take a while)")
     for channel in data.T:
         print('Processing channel {} ...'.format(i))
-        tf, _, ctr_freq, _ = process_nwb.wavelet_transform.wavelet_transform(channel.reshape(-1, 1), fs, filters='rat', hg_only=False, X_fft_h=None, npad=None)
+        tf, _, ctr_freq, _ = wavelet_transform.wavelet_transform(channel.reshape(-1, 1), fs, filters='rat', hg_only=False, X_fft_h=None, npad=None)
         
         if(zscore): # Z-score channel
-            tf = zscore_signal(tf)
+            ztf = np.zeros(tf.shape)
+            for freq_band in range(len(ctr_freq)):
+                ztf[:, 0, freq_band] = zscore_signal(np.abs(tf[:, 0, freq_band]))
 
-        tf_data.append(np.abs(tf))
+
+        tf_data.append(np.abs(ztf))
         i += 1
         
     # Stacked wavelet transformed channels back together
@@ -56,9 +59,9 @@ def wavelet_transform(data, fs, fsds=1000, zscore=True):
     
     return tf_data, ctr_freq
 
-def zscore_signal(signal, mean=None, std=None):
+def zscore_signal(signal, axis=0, mean=None, std=None):
     if mean is None:
-        mean = np.mean(signal)
+        mean = np.mean(signal, axis=axis)
     if std is None:
-        std = np.std(signal)
+        std = np.std(signal, axis=axis)
     return (signal - mean)/std
