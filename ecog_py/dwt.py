cc61e50172617928045c64b6e0b18f73fb88f6e6
Kevin <kevin.qi@berkeley.edu>
2021-01-21 17:37:09 -0800

    Add downsampling and hg_only options to dwt and pass test suite

--- ecog_py/dwt.py
+++ ecog_py/dwt.py
@@ -7,8 +7,9 @@ Performs discrete wavelet transforms.
 
 from process_nwb import wavelet_transform
 import numpy as np
+import scipy
 
-def dwt(data, fs, fsds=1000, zscore=True):
+def dwt(data, fs, fsds=1000, zscore=True, hg_only=False):
     """
     Performs Discrete Wavelet Transform on multi-channel ECoG data. The processing is done channel by channel to reduce RAM usage.
     
@@ -22,6 +23,8 @@ def dwt(data, fs, fsds=1000, zscore=True):
         Post-wavelet transform downsampled frequency (to reduce memory usage).
     zscore : bool, default True
         If True, zscore each frequency band. (Enables comparison across frequency bands despite 1/f power falloff)
+    hg_only : bool, default False
+        If True, only compute high gamma bands (70-150hz)
         
     Returns
     -------
@@ -41,17 +44,22 @@ def dwt(data, fs, fsds=1000, zscore=True):
     print("Wavelet transforming per channel... (This may take a while)")
     for channel in data.T:
         print('Processing channel {} ...'.format(i))
-        tf, _, ctr_freq, _ = wavelet_transform.wavelet_transform(channel.reshape(-1, 1), fs, filters='rat', hg_only=False, X_fft_h=None, npad=None)
-        
+        tf, _, ctr_freq, _ = wavelet_transform.wavelet_transform(channel.reshape(-1, 1), fs, filters='rat', hg_only=hg_only, X_fft_h=None, npad=None)
+
         if(zscore): # Z-score channel
-            ztf = np.zeros(tf.shape)
+            ztf = np.zeros((int(np.max(tf.shape)/int(fs/int(fsds))), 1, len(ctr_freq)))
             for freq_band in range(len(ctr_freq)):
-                ztf[:, 0, freq_band] = zscore_signal(np.abs(tf[:, 0, freq_band]))
-
-
-        tf_data.append(np.abs(ztf))
+                z = zscore_signal(np.abs(tf[:,0, freq_band]))
+                z_ds = scipy.signal.resample(z, int(np.max(z.shape)/int(fs/int(fsds))), axis=0) # Downsample
+                ztf[:, 0, freq_band] = z_ds
+            tf_data.append(ztf)
+        else: # No Z-score
+            tf = scipy.signal.resample(tf, int(np.max(tf.shape)/int(fs/int(fsds))), axis=0) # Downsample
+            tf_data.append(np.abs(tf))
         i += 1
-        
+
+
+    
     # Stacked wavelet transformed channels back together
     tf_data = np.concatenate(tf_data, axis=1)
     # Permute the axis to get shape (num_channels, num_bands, num_samples)
@@ -59,7 +67,7 @@ def dwt(data, fs, fsds=1000, zscore=True):
     # Flip the bands ordering so that low frequencies are at the bottom
     tf_data = np.flip(tf_data, axis=1)
     
-    return tf_data, ctr_freq
+    return tf_data, np.flip(ctr_freq)
 
 def zscore_signal(signal, axis=0, mean=None, std=None):
     if mean is None:
